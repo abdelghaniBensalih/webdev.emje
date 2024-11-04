1028145b7366ef569979b650748fead68236e95b
a.bensalih <abdelghaniben40@gmail.com>
2024-11-04 13:53:39 +0100

    update editUrl in docusaurus.config.js to include the branch in the repository link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/abdelghaniBensalih/webdev.emje',
+            'https://github.com/abdelghaniBensalih/webdev.emje/tree/master',
         },
         blog: {
           showReadingTime: true,
@@ -55,7 +55,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/abdelghaniBensalih/webdev.emje',
+            'https://github.com/abdelghaniBensalih/webdev.emje/tree/master',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
