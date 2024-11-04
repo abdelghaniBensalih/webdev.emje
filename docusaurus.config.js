d1fa0c7f1a656974ac19376c5a3aea3dd149c167
a.bensalih <abdelghaniben40@gmail.com>
2024-11-04 11:58:37 +0100

    added a schedule can be edited

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,10 +74,10 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'EMJE',
+        title: 'Home',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo2.svg',
         },
         items: [
           {
